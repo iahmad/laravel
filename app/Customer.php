957cc3151a80b8de83436da46ba28802c7eb80f7
Ahmad <hi@ahmad.im>
2017-05-05 06:17:21 +0300

    test push new update

--- app/Customer.php
+++ app/Customer.php
@@ -13,9 +13,9 @@ class Customer extends Model
   protected $fillable = [
       'name', 'email', 'password', 'api_token',
   ];
- 
+
   protected $hidden = [
-      'password', 'remember_token',
+      'password', 'remember_token', 
   ];
 
 }
